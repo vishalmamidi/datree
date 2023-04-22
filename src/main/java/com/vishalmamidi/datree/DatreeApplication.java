a4f79519a5d3940bac06da1c42777acc7325cb04
vishalmamidi1 <vishal.mamidi@valuemomentum.com>
2023-04-22 20:18:54 +0530

    initial commit

---
+++ src/main/java/com/vishalmamidi/datree/DatreeApplication.java
@@ -0,0 +1,13 @@
+package com.vishalmamidi.datree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishalmamidi/datree/DatreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishalmamidi.datree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
